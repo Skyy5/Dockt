ca5189b2240e4c2d5d8b8e27ab345d1de423bad7
Skyy5 <abi.oyewole@hotmail.com>
2013-11-19 22:00:56 -0800

    Added files from Calendar project

---
+++ dockt-test/CKAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CKAppDelegate.h
+//  DOCKT
+//
+//  Created by Abi on 2013-11-19.
+//  Copyright (c) 2013 Team Yeezus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CKViewController;
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CKViewController *viewController;
+
+@end
---
+++ dockt-test/CKViewController.h
@@ -0,0 +1,13 @@
+//
+//  CKViewController.h
+//  DOCKT
+//
+//  Created by Abi on 2013-11-19.
+//  Copyright (c) 2013 Team Yeezus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKViewController : UIViewController
+
+@end
